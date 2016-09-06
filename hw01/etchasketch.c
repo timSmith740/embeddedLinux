26ee045156c508661be732d17e42b2f75c34e222
Tim L. Smith <timothys740@gmail.com>
2016-09-05 22:57:56 -0400

    Checks on inputs for and prints where the cursor will be moved

--- hw01/etchasketch.c
+++ hw01/etchasketch.c
@@ -5,19 +5,33 @@
 #include <stdio.h>
 
 /* Definitions */
-#define left 'l';
-#define right 'r';
-#define up 'u';
-#define down 'd';
-#define quit 'q';
-
+char left  = 'l';
+char right ='r';
+char up    ='u';
+char down  ='d';
+char quit  = 'q';
 int boardSize = 0;
 
 void main(int size){
 	char  direction;
 
 	boardSize = size;
-
-	direction = getchar();
-	printf("%c\n", direction);
+	while(1) {
+		direction = getchar();
+		if (direction == left) {
+			printf("left!\n");
+		} else if (direction == right) {
+			printf("right!\n");
+		}
+		else if (direction == up) {
+			printf("up!\n");
+		}
+		else if (direction == down) {
+			printf("down!\n");
+		}
+		else if (direction == quit) {
+			printf("quit!\n");
+			break;
+		}
+	}	
 }
