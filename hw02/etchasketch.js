42a79408c31a293e76ea0fa3f5daec4377186c53
Timothy Smith <timothys740@gmail.com>
2016-09-09 04:56:20 +0000

    Board is marked correctly, though the interrupts run when it first goes through

--- hw02/etchasketch.js
+++ hw02/etchasketch.js
@@ -4,9 +4,10 @@ var button1 = 'P9_11';
 var button2 = 'P9_13';
 var button3 = 'P9_16';
 var button4 = 'P9_17';
+var button5 = 'P9_21';
 var xPos = 0;
 var yPos = 0;
-var width = 1;
+var width = 5;
 var board = [];
 
 for(i = 0; i < width; i++){
@@ -20,6 +21,7 @@ b.pinMode(button1, b.INPUT, 7, 'pulldown');
 b.pinMode(button2, b.INPUT, 7, 'pulldown');
 b.pinMode(button3, b.INPUT, 7, 'pulldown');
 b.pinMode(button4, b.INPUT, 7, 'pulldown');
+b.pinMode(button5, b.INPUT, 7, 'pulldown');
 
 b.attachInterrupt(button1, true,
 	b.FALLING, updateUp);
@@ -29,10 +31,14 @@ b.attachInterrupt(button3, true,
 	b.FALLING, updateDown);
 b.attachInterrupt(button4, true,
 	b.FALLING, updateLeft);
+b.attachInterrupt(button5, true,
+	b.FALLING, clear);
 
 function updateLeft(){
+    console.log("left");
     if (xPos !== 0){
         xPos--;
+        board[yPos][xPos] = 'X';
         updateBoard();
     } else {
         xPos = 0;
@@ -40,8 +46,10 @@ function updateLeft(){
 }
 
 function updateRight(){
-    if (xPos !== 0){
+    console.log("right");
+    if (xPos !== width){
         xPos++;
+        board[yPos][xPos] = 'X';
         updateBoard();
     } else {
         xPos = 0;
@@ -49,8 +57,10 @@ function updateRight(){
 }
 
 function updateUp(){
+    console.log("up");
     if (yPos !== 0){
         yPos--;
+        board[yPos][xPos] = 'X';
         updateBoard();
     } else {
         yPos = 0;
@@ -58,8 +68,10 @@ function updateUp(){
 }
 
 function updateDown(){
-    if (yPos !== 0){
-        yPos--;
+    console.log("down");
+    if (yPos !== width){
+        yPos++;
+        board[yPos][xPos] = 'X';
         updateBoard();
     } else {
         yPos = 0;
@@ -68,8 +80,17 @@ function updateDown(){
 
 function updateBoard(){
     for(i = 0; i < width; i++){
-        for (j =0; j < width; j++){
-            console.log(board[i]);
+        console.log(board[i]);
     }
 }
+
+function clear(){
+    for(i = 0; i < width; i++){
+        for(j = 0; j < width; j++){
+            board[i][j] = 0;
+        }
+    }
+    xPos = 0;
+    yPos = 0;
+    updateBoard();
 }
