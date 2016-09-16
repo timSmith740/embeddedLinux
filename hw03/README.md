ba7d4ab36a151a8f1da36a5a38fec49277ff802b
Timothy Smith <timothys740@gmail.com>
2016-09-16 04:17:18 +0000

    Added files for temp sensors

--- hw03/README.md
+++ hw03/README.md
@@ -1,1 +1,9 @@
 # embeddedLinux
+My etchasketch.js displays on the LED matrix. There are five buttons they are hooked up to 
+pins 11,13,16,17,and 21. 11,13,16,17 move the cursor and 21 will clear the screen and 
+put the cursor in the upper left corner.
+
+You can run my temp.sh file and it will read a temp101 sensor at address 0x48.
+
+If you run my tempSetup.sh file it will prepare temp101sensor on addresses 0x48 and 0x49.
+It prints the temperature after getting an alert, but it does not work fully yet.
---
+++ hw03/tempAlert.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+/* Variable declarations */
+var b = require('bonescript');
+var i2c = require('i2c');
+var alert1 = 'P9_12';
+var alert2 = 'P9_14';
+var temp1 = new i2c(0x48, {device: '/dev/i2c-2'});
+var temp2 = new i2c(0x49, {device: '/dev/i2c-2'});
+var temp = 0;
+
+b.pinMode(alert1, b.INPUT, 7, 'disabled');
+b.pinMode(alert2, b.INPUT, 7, 'disabled');
+
+b.attachInterrupt(alert1, true,
+	b.RISING, alertOne);
+b.attachInterrupt(alert2, true,
+	b.RISING, alertTwo);
+	
+function alertOne(){
+    console.log("alert1");
+    temp = temp1.readByte(function(err, data){});
+    console.log(((temp*9)/5)+32);
+}
+function alertTwo(){
+    console.log("alert2");
+    temp = temp2.readByte(function(err){});
+    console.log(((temp*9)/5)+32);
+}
