27bf1d1a2506486f5cefa2089c128de86d60044f
Timothy Smith <timothys740@gmail.com>
2016-09-20 19:07:12 +0000

    changed a little how temp006 works

--- hw03/temp006sensor.sh
+++ hw03/temp006sensor.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-temp=$(i2cget -y 2 0x40 1)
+temp=$(i2cget -y 2 0x40 1 w)
+temp2=$((($temp) >> 2))
 echo $temp
-temp2=$((($temp)*9))
-temp3=$((($temp2)/5))
-temp4=$((($temp3)+32))
+echo $temp2
+temp3=$((($temp2)*9))
+temp4=$((($temp3)/5))
+temp5=$((($temp4)+32))
 echo $temp4
