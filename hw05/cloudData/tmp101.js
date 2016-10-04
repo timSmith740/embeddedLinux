9d04a663bf6d962dbd456fd472292cb3a1014595
Tim Smith <timothys740@gmail.com>
2016-10-04 18:56:15 +0000

    Added some comments to the file

--- hw05/cloudData/tmp101.js
+++ hw05/cloudData/tmp101.js
@@ -26,6 +26,8 @@ var temp = [];
 
 // Read the temp sensors
 for(var i=0; i<tmp101.length; i++) {
+    // Checking if we are using the tmp006 sensor where
+    // temp data is in a different register
     if (i === 2){
         temp[i] = sensor.readByteSync(tmp101[i], 0x1);
     } else {
@@ -35,7 +37,7 @@ for(var i=0; i<tmp101.length; i++) {
     console.log("temp: %dC, %dF (0x%s)", temp[i], temp[i]*9/5+32, tmp101[i].toString(16));
 }
 
-// Substitute in the temperatures
+// Substitute in the temperatures and convert to F
 var url = util.format(urlBase, temp[0]*9/5+32, temp[1]*9/5+32, temp[2]*9/5+32);
 console.log("url: ", url);
 
