a8b3f9948aa7a8148fe9227dc3befe70780147e4
Tim Smith <timothys740@gmail.com>
2016-10-26 03:08:53 +0000

    Finished javascript file

---
+++ hw07/gpioThru.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+var b = require('bonescript');
+var button1 = 'P9_28';
+var LED1 = 'P9_29';
+
+var state1 = b.HIGH;
+
+b.pinMode(button1, b.INPUT, 7, 'pulldown');
+b.pinMode(LED1, b.OUTPUT);
+
+b.attachInterrupt(button1, true,
+	b.CHANGE, flash1);
+	
+console.log("Ready");
+	
+function flash1(x){
+	if (x.attached === true){
+        return;
+    }
+	b.digitalWrite(LED1, state1);
+	if(state1 === b.HIGH) {
+		state1 = b.LOW;
+	} else {
+		state1 = b.HIGH
+	}
+}
